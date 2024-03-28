fba9fd0ec93af6deee050cd6d75a5c9a7f1dfa4e
Vaibhav77e <111438234+Vaibhav77e@users.noreply.github.com>
2024-03-28 22:51:28 +0530

    added models

---
+++ models/user_models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        validate : [validator.isEmail,'Please enter a valid email address']
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength:[8,"The password must be of 8 characters"]
+    }
+},
+{
+    timestamps:true,
+}
+);
+
+const userModel = mongoose.model('User',userSchema);
+
+module.exports =userModel;
+
